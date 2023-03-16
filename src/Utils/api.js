3dcaf2689992ecbe38086cf30e5e40216dbde7c3
Wiggy93 <110779323+Wiggy93@users.noreply.github.com>
2023-03-16 18:31:53 +0000

    all users added to /users page

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,44 +1,67 @@
-import axios from 'axios'
+import axios from "axios";
 
 const ncGamesAPI = axios.create({
-    baseURL: "https://games-reviews-and-comments.onrender.com/api/"
-})
+  baseURL: "https://games-reviews-and-comments.onrender.com/api/",
+});
 
 export const getReviews = (category, sortBy, order) => {
-    return ncGamesAPI.get('/reviews', {
-        params: {
-            category: category,
-            sort_by: sortBy,
-            order: order
-        }
-    }).then(({data})=>{return data})
-}
+  return ncGamesAPI
+    .get("/reviews", {
+      params: {
+        category: category,
+        sort_by: sortBy,
+        order: order,
+      },
+    })
+    .then(({ data }) => {
+      return data;
+    });
+};
 
 export const getReviewById = (review_id) => {
-    return ncGamesAPI.get(`/reviews/${review_id}`).then((data)=> {return data})
-}
+  return ncGamesAPI.get(`/reviews/${review_id}`).then((data) => {
+    return data;
+  });
+};
 
 export const getCommentsById = (review_id) => {
-    return ncGamesAPI.get(`/reviews/${review_id}/comments`).then(({data})=>{
-        return data
-    })
-}
+  return ncGamesAPI.get(`/reviews/${review_id}/comments`).then(({ data }) => {
+    return data;
+  });
+};
 
 export const updateVotes = (review_id, voteIncrement) => {
-    return ncGamesAPI.patch(`/reviews/${review_id}`, {inc_votes: `${voteIncrement}`})
-        .then(({data})=>{return data})
-}
+  return ncGamesAPI
+    .patch(`/reviews/${review_id}`, { inc_votes: `${voteIncrement}` })
+    .then(({ data }) => {
+      return data;
+    });
+};
 
 export const updateCommentVotes = (comment_id, voteIncrement) => {
-    return ncGamesAPI.patch(`/comments/${comment_id}`, {inc_votes: `${voteIncrement}`})
-        .then(({data})=>{return data})
-}
+  return ncGamesAPI
+    .patch(`/comments/${comment_id}`, { inc_votes: `${voteIncrement}` })
+    .then(({ data }) => {
+      return data;
+    });
+};
 
 export const getCategories = () => {
-    return ncGamesAPI.get('/categories').then(({data})=>{return data})
-}
+  return ncGamesAPI.get("/categories").then(({ data }) => {
+    return data;
+  });
+};
 
 export const postComment = (review_id, commentBody) => {
-    return ncGamesAPI.post(`reviews/${review_id}/comments`, commentBody)
-        .then(({data})=>{return data})
-}
+  return ncGamesAPI
+    .post(`reviews/${review_id}/comments`, commentBody)
+    .then(({ data }) => {
+      return data;
+    });
+};
+
+export const getUsers = () => {
+  return ncGamesAPI.get("/users").then(({ data }) => {
+    return data.allUsers;
+  });
+};
