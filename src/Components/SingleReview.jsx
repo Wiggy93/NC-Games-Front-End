72342975388da3c9fcdb61fc5b00f69173487473
Wiggy93 <110779323+Wiggy93@users.noreply.github.com>
2023-03-17 12:30:55 +0000

    users styling, vote button disabled after clicking on specific review/comment

--- src/Components/SingleReview.jsx
+++ src/Components/SingleReview.jsx
@@ -1,87 +1,120 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
-import { getReviewById, updateVotes } from '../Utils/api';
-import { dateConverter } from '../Utils/utils';
-import { Comments } from './Comments';
-import { ErrorPage } from './ErrorPage';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getReviewById, updateVotes } from "../Utils/api";
+import { dateConverter } from "../Utils/utils";
+import { Comments } from "./Comments";
+import { ErrorPage } from "./ErrorPage";
 
-import styles from '../CSS/SingleReview.module.css'
+import styles from "../CSS/SingleReview.module.css";
 
-export const SingleReview = ({currentReview, setCurrentReview, currentUser}) => {
-   const { reviewid } = useParams();
-   const [newTime, setNewTime] = useState("")
-   const [err, setErr] = useState(null)
-   const [isLoading, setIsLoading] = useState(false);
-   const [errorMessage, setErrorMessage] = useState({display: "none"})
+export const SingleReview = ({
+  currentReview,
+  setCurrentReview,
+  currentUser,
+}) => {
+  const { reviewid } = useParams();
+  const [newTime, setNewTime] = useState("");
+  const [err, setErr] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState({ display: "none" });
+  const [haveVotedIds, setHaveVotedIds] = useState([]);
 
-   useEffect(()=>{
-    setIsLoading(true)
+  useEffect(() => {
+    setIsLoading(true);
     getReviewById(Number(reviewid))
-    .then(({data}) => {
-        setCurrentReview(data.reviewObj[0])
-        setIsLoading(false)
-    })
-    .catch((err)=>{
+      .then(({ data }) => {
+        setCurrentReview(data.reviewObj[0]);
+        setIsLoading(false);
+      })
+      .catch((err) => {
         console.log(err);
         setErr(err);
         setIsLoading(false);
-    })
-    },[currentReview])
-    
-    useEffect(()=>{
-        setIsLoading(true)
-        const changeDateFormat = dateConverter(currentReview.created_at);
-        setNewTime(changeDateFormat)
-        setIsLoading(false)
-    },[currentReview])
+      });
+  }, [currentReview]);
 
-    const updateVoteButton = (review_id, e) =>{
-      
-        setCurrentReview((currentReview)=>{
-            return currentReview.votes = currentReview.votes + Number(e)
-               })
-        
-        setErrorMessage({display: "none"})
-        updateVotes(review_id, Number(e))
-        
-        .catch((err)=>{
-            console.log(err);
-            setCurrentReview((review)=>{
-                return currentReview.votes = currentReview.votes - Number(e)
-                   })
-                
-            setErrorMessage({display: "block" })
-                })
-    }
+  useEffect(() => {
+    setIsLoading(true);
+    const changeDateFormat = dateConverter(currentReview.created_at);
+    setNewTime(changeDateFormat);
+    setIsLoading(false);
+  }, [currentReview]);
 
-    if(isLoading) return <p>Loading review...</p>
+  const updateVoteButton = (review_id, e) => {
+    setCurrentReview((currentReview) => {
+      return (currentReview.votes = currentReview.votes + Number(e));
+    });
 
-    if (err) {
-        return <ErrorPage err={err}/>
-    }
+    setErrorMessage({ display: "none" });
+    updateVotes(review_id, Number(e));
+    setHaveVotedIds([...haveVotedIds, review_id]).catch((err) => {
+      console.log(err);
+      setCurrentReview((review) => {
+        return (currentReview.votes = currentReview.votes - Number(e));
+      });
 
-     return (
-     
-        <section className={styles.reviewcard}>
-            <h2 id={styles.title} className={styles.gridItem}>{currentReview.title}</h2>
-            <h3 id={styles.username} className={styles.gridItem}>{currentReview.owner}</h3>
-            <p id={styles.timeOfPost} className={styles.gridItem}>{newTime}</p>
-            <p id={styles.category} className={styles.gridItem}>Category: {currentReview.category}</p>
-            <img src={currentReview.review_img_url} alt={`The game ${currentReview.title}`} className={styles.gridItem}></img>
+      setErrorMessage({ display: "block" });
+    });
+  };
 
-            <p id={styles.body} className={styles.gridItem}>{currentReview.review_body} </p>
-            <br></br>
-            <p id={styles.votes} className={styles.gridItem} >Review Votes: {currentReview.votes}</p>
-            
-            <div id={styles.voteButtons}>
-            <button  value={1} onClick={(e) => updateVoteButton(currentReview.review_id, e.target.value)}>Vote: +1</button> 
-                    {/* for accessibility put thumbs up/down for +1 or -1 */}
-            <button value={-1} onClick={(e) => updateVoteButton(currentReview.review_id, e.target.value)}>Vote: -1</button>
-            </div>
-            
-                    
-            <p style={errorMessage}>Error updating review votes</p>
-           <Comments currentUser={currentUser}/>
-        </section>
-    )
-}
+  if (isLoading) return <p>Loading review...</p>;
+
+  if (err) {
+    return <ErrorPage err={err} />;
+  }
+
+  return (
+    <section className={styles.reviewcard}>
+      <h2 id={styles.title} className={styles.gridItem}>
+        {currentReview.title}
+      </h2>
+      <h3 id={styles.username} className={styles.gridItem}>
+        {currentReview.owner}
+      </h3>
+      <p id={styles.timeOfPost} className={styles.gridItem}>
+        {newTime}
+      </p>
+      <p id={styles.category} className={styles.gridItem}>
+        Category: {currentReview.category}
+      </p>
+      <img
+        src={currentReview.review_img_url}
+        alt={`The game ${currentReview.title}`}
+        className={styles.gridItem}
+      ></img>
+
+      <p id={styles.body} className={styles.gridItem}>
+        {currentReview.review_body}{" "}
+      </p>
+      <br></br>
+      <p id={styles.votes} className={styles.gridItem}>
+        Review Votes: {currentReview.votes}
+      </p>
+
+      <div id={styles.voteButtons}>
+        <button
+          value={1}
+          onClick={(e) =>
+            updateVoteButton(currentReview.review_id, e.target.value)
+          }
+          disabled={haveVotedIds.includes(currentReview.review_id)}
+        >
+          Vote: +1
+        </button>
+        {/* for accessibility put thumbs up/down for +1 or -1 */}
+        <button
+          value={-1}
+          onClick={(e) =>
+            updateVoteButton(currentReview.review_id, e.target.value)
+          }
+          disabled={haveVotedIds.includes(currentReview.review_id)}
+        >
+          Vote: -1
+        </button>
+      </div>
+
+      <p style={errorMessage}>Error updating review votes</p>
+      <Comments currentUser={currentUser} />
+    </section>
+  );
+};
--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,24 +1,40 @@
 import { useEffect, useState } from "react";
 import { getUsers } from "../Utils/api";
 import styles from "../CSS/Users.module.css";
+import { ErrorPage } from "./ErrorPage";
 
 export const Users = () => {
   const [loading, setIsLoading] = useState(false);
   const [users, setUsers] = useState([]);
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
     setIsLoading(true);
-    getUsers().then((data) => {
-      setUsers(data);
-      setIsLoading(false);
-    });
+    getUsers()
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setErr(err);
+        setIsLoading(false);
+      });
   }, []);
 
   if (loading) {
     return <p>Loading Users...</p>;
   }
+
+  if (err) {
+    return (
+      <div>
+        <ErrorPage err={err} />
+      </div>
+    );
+  }
   return (
-    <section>
+    <section className={styles.section}>
       <h3>All Users</h3>
       <ul className={styles.usersBox}>
         {users.map((user) => {
